9b45f9b2a9d980fdcc5ef4c03233845e9e64b099
Nidhin Xavier <132472527+NDX7@users.noreply.github.com>
2024-04-17 10:54:32 +0530

    Update factorialusingfunction.c

--- factorialusingfunction.c
+++ factorialusingfunction.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 
-int fact(int);
+int fact(int n);
 int main(){
 
-int n,f;
-printf("Enter number to find factorial");
-scanf("%d",&n);
+    int n,f;
+    printf("Enter number to find factorial\n");
+    scanf("%d",&n);
 
-f=fact(n);
-printf("factorial is %d",n);
-return 0;
+    f=fact(n);
+    printf("factorial is %d",f);
+    return 0;
 }
 
 int fact(int n){
-	if(n==1||n==0){
-	printf("factorial is 1");}
-	else{
-		return n*fact(n-1);
-		}}
+    if(n==1||n==0)
+        return 1;
+    else
+        return (n*fact(n-1));//using recursion
+}
